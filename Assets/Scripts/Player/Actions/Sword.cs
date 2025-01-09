efaa36f954d9e1a102969eafb1742227a342ec2e
whitefri2z <white.fri2z12@gmail.com>
2025-01-09 12:34:45 +0100

    Tweaks to the prefabs

--- Assets/Scripts/Player/Actions/Sword.cs
+++ Assets/Scripts/Player/Actions/Sword.cs
@@ -1,13 +1,22 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
+    // All Colliders
     private CapsuleCollider _playerBodyRef;
+    private BoxCollider _damageArea;
+    
+    // Enemy Refs
+    private List<Ennemy> _allEnemies = new();
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _playerBodyRef = transform.parent.GetComponent<CapsuleCollider>();
+        _damageArea = gameObject.GetComponent<BoxCollider>();
+        Destroy(gameObject, 3f);
     }
 
     // Update is called once per frame
@@ -22,6 +31,27 @@ public class Sword : MonoBehaviour
 
     private void SwordAttack()
     {
-        
+        if (_allEnemies == null) return;
+        if (_allEnemies.Count <= 0) return;
+        foreach (var curEnemy in _allEnemies)
+        {
+            print("Attack Worked and hit " + curEnemy.name + " successfully.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            _allEnemies.Add(other.gameObject.GetComponent<Ennemy>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            _allEnemies.Remove(other.gameObject.GetComponent<Ennemy>());
+        }
     }
 }
