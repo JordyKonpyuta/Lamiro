de16227245799f8661942b84a3c5918b03a08e88
Justine C <83780776+JordyKonpyuta@users.noreply.github.com>
2025-01-10 11:11:00 +0100

    Finish setting up Main Menu
    - Create scene
    - Place elements
    - add functions on elements

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,14 +1,55 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    
     public GameObject settingsPanel;
+    public Text volumeText;
+    public Slider volumeSlider;
+    public Toggle fullScreenToggle;
+    public Dropdown qualityDropdown;
+
+    public string levelToLoad;
+
+    private void Start()
+    {
+        SetVolume();
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
 
     public void SettingsButton()
     {
-        if (settingsPanel.activeSelf)
-            settingsPanel.SetActive(false);
-        else 
-            settingsPanel.SetActive(true);
+        settingsPanel.SetActive(!settingsPanel.activeSelf);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void SetVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        volumeText.text = (volumeSlider.value * 100).ToString("0");
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+    }
+
+    public void SetQuality()
+    {
+        PlayerPrefs.SetInt("masterQuality", qualityDropdown.value);
+        QualitySettings.SetQualityLevel(qualityDropdown.value);
+    }
+
+    public void SetFullScreen()
+    {
+        PlayerPrefs.SetInt("masterFullscreen", fullScreenToggle.isOn ? 1 : 0);
+        Screen.fullScreen = fullScreenToggle.isOn;
     }
 }
